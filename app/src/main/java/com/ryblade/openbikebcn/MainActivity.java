9cf58a673911f0f4b0ae20161b78a1dc1ac3f81b
Pau Madrero Pardo <Pau Madrero Pardo>
2015-11-22 18:28:50 +0100

    my location working + stations markers

--- app/src/main/java/com/ryblade/openbikebcn/MainActivity.java
+++ app/src/main/java/com/ryblade/openbikebcn/MainActivity.java
@@ -1,13 +1,20 @@
 package com.ryblade.openbikebcn;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.PermissionRequest;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.overlays.Marker;
@@ -15,40 +22,19 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 import com.ryblade.openbikebcn.FetchAPITask;
+import com.ryblade.openbikebcn.Model.Station;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        MapView mapView = (MapView) findViewById(R.id.map);
-
-
-        IMapController mapController = mapView.getController();
-        mapController.setZoom(15);
-
-        GeoPoint startPoint = new GeoPoint(41.3833, 2.1833);
-        mapController.setCenter(startPoint);
-
-        Drawable dr = new BitmapDrawable(((View) getResources().getLayout(R.layout.marker_layout)).getDrawingCache());
-
-        Marker startMarker = new Marker(mapView);
-        startMarker.setPosition(startPoint);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setIcon(dr);
-        startMarker.setTitle("Start point");
-        mapView.getOverlays().add(startMarker);
-
-        mapView.invalidate();
-
 
         this.updateBikesDatabase();
-
-
+        Intent intent = new Intent(this, OsmActivity.class);
+        startActivity(intent);
     }
 
     private void updateBikesDatabase() {
