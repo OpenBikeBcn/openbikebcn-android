f18bdab5bfee88a5f4e495526240454dfb18f8a8
Pau Madrero Pardo <Pau Madrero Pardo>
2015-11-28 10:28:48 +0100

    favourites graphical interface done

--- app/src/main/java/com/ryblade/openbikebcn/Fragments/FavoritesFragment.java
+++ app/src/main/java/com/ryblade/openbikebcn/Fragments/FavoritesFragment.java
@@ -7,8 +7,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.ryblade.openbikebcn.MainActivity;
+import com.ryblade.openbikebcn.Model.Station;
 import com.ryblade.openbikebcn.R;
+import com.ryblade.openbikebcn.StationArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by alexmorral on 25/11/15.
@@ -29,6 +35,7 @@ public class FavoritesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.favorites_fragment, container, false);
+        init(rootView);
 
         return rootView;
     }
@@ -36,5 +43,11 @@ public class FavoritesFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private void init(View v) {
+        ListView lv = ((ListView) v.findViewById(R.id.favouritesList));
+        StationArrayAdapter saa = new StationArrayAdapter(getContext(), new ArrayList<Station>());
+        lv.setAdapter(saa);
     }
 }
