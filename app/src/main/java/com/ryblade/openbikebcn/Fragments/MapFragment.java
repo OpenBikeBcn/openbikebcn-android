1addf8bed82981ddd07f7798ddd529cf4a879b09
alexmorral <alexmorral@gmail.com>
2015-11-29 17:58:41 +0100

    Routes working

--- app/src/main/java/com/ryblade/openbikebcn/Fragments/MapFragment.java
+++ app/src/main/java/com/ryblade/openbikebcn/Fragments/MapFragment.java
@@ -303,7 +303,7 @@ public class MapFragment extends Fragment implements LocationListener, OnRouteFe
     }
 
     public void endRouteClicked() {
-        Log.v(LOG_TAG,"End route clicked");
+        Log.v(LOG_TAG, "End route clicked");
         if (currentStartStation != null) {
             currentEndStation = currentStation;
 
@@ -322,84 +322,27 @@ public class MapFragment extends Fragment implements LocationListener, OnRouteFe
 
 
     public void drawRoute() {
-        RoadManager roadManager = new OSRMRoadManager();
-
-
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-
         GeoPoint startPoint = new GeoPoint(currentStartStation.getLatitude(), currentStartStation.getLongitude());
-        waypoints.add(startPoint);
+
         GeoPoint endPoint = new GeoPoint(currentEndStation.getLatitude(), currentEndStation.getLongitude());
-        waypoints.add(endPoint);
-
-        Road road = roadManager.getRoad(waypoints);
-
-        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, getActivity());
-
-        mapView.getOverlays().add(roadOverlay);
-
-        mapView.invalidate();
 
         new FetchRouteAPITask(getActivity(), this).execute(startPoint, endPoint);
-
     }
-
-
-
-    public static List<LatLng> decode(final String encodedPath) {
-        int len = encodedPath.length();
-
-        // For speed we preallocate to an upper bound on the final length, then
-        // truncate the array before returning.
-        final List<LatLng> path = new ArrayList<LatLng>();
-        int index = 0;
-        int lat = 0;
-        int lng = 0;
-
-        while (index < len) {
-            int result = 1;
-            int shift = 0;
-            int b;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lat += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            result = 1;
-            shift = 0;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lng += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            path.add(new LatLng(lat * 1e-6, lng * 1e-6));
-        }
-
-        return path;
-    }
-
 
 
     public void OnRouteFetched(Route route) {
-        Drawable darkRed = getResources().getDrawable(R.drawable.darkred_marker);
         if (route != null) {
+            RoadManager roadManager = new OSRMRoadManager();
+            ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
             for (LatLng point : route.getRoute()) {
-                Drawable icon;
-                icon = darkRed;
-
-                Marker stationMarker = new Marker(mapView);
-                stationMarker.setPosition(new GeoPoint(point.getLat(), point.getLon()));
-                stationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-                stationMarker.setIcon(icon);
-                stationMarker.setTitle(String.valueOf(2));
-                stationMarker.setSnippet(String.valueOf(2));
-
-                mapView.getOverlays().add(stationMarker);
+                waypoints.add(new GeoPoint(point.getLat(), point.getLon()));
             }
+
+            Road road = roadManager.getRoad(waypoints);
+
+            Polyline roadOverlay = RoadManager.buildRoadOverlay(road, getActivity());
+
+            mapView.getOverlays().add(roadOverlay);
 
             mapView.invalidate();
 
